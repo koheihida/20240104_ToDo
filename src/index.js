6118b74c0116be3457a0a2abd3a976724d6b23cd
koheihida <koheihida04@gmail.com>
2024-01-05 01:35:10 +0900

    html/cssのクラス名修正

--- src/index.js
+++ src/index.js
@@ -17,14 +17,23 @@ const onClickAdd = () => {
   p.className = "todo-item";
   p.innerText = inputText;
 
+  // 完了ボタンの実装
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  // 削除ボタンの実装
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
   // liタグの子要素に各要素を設定
   div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   li.appendChild(div);
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(li);
 
-  console.log(li);
+  console.log(completeButton);
 }
 
 document.getElementById("add-button").addEventListener("click", onClickAdd);
