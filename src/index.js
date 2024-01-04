ec64b7218e76dc2a2fe34f56ac4c40913173d61d
koheihida <koheihida04@gmail.com>
2024-01-05 02:28:30 +0900

    完了ボタンの実装

--- src/index.js
+++ src/index.js
@@ -21,7 +21,14 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    const moveTarget = completeButton.closest("li");
+    completeButton.nextElementSibling.remove();
+    completeButton.remove();
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    moveTarget.firstElementChild.appendChild(backButton);
+
+    document.getElementById("complete-list").appendChild(moveTarget);
   });
   // 削除ボタンの実装
   const deleteButton = document.createElement("button");
